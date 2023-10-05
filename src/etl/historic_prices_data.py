ad4b035dad56ad2277c5c668609f5181b9879637
Rajat Jain <rajat.apple007@gmail.com>
2023-10-05 11:24:47 -0700

    Saving historic prices data to SQLite instance

--- src/etl/historic_prices_data.py
+++ src/etl/historic_prices_data.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import pandas as pd
+import sqlite3
 
 # Define the ticker symbol for Gold Futures
 tickerSymbol = "GC=F"
@@ -12,7 +13,13 @@ historicalData = goldData.history(period="max")
 df = pd.DataFrame(historicalData)
 
 # Data cleaning
-df = df.drop(columns=["Dividends","Stock Splits"])
+df = df.drop(columns=["Dividends", "Stock Splits"])
 
-# Save data
-df.to_csv("data/historicPrices.csv")
+# Connect to the SQLite database
+conn = sqlite3.connect("data/historic_prices.db")
+
+# Save the DataFrame to the SQLite database
+df.to_sql("historic_prices", conn, if_exists="replace", index=False)
+
+# Close the database connection
+conn.close()
